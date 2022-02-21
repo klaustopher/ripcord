67332537088051940e4417e49f238d035252fb0f
Klaus Zanders <code@kgz.me>
2022-02-21 22:46:13 +0100

    Switch to Coveralls GH action and don't run rubocop on CI

--- Gemfile
+++ Gemfile
@@ -5,7 +5,8 @@ source "https://rubygems.org"
 # Specify your gem's dependencies in ripcord.gemspec
 gemspec
 
-gem "coveralls", require: false
+gem 'simplecov'
+gem 'simplecov-lcov', '~> 0.8.0'
 gem "rake", "~> 13.0"
 gem "rspec", "~> 3.0"
 gem "rubocop", "~> 1.21"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,21 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "webmock/rspec"
 WebMock.disable_net_connect!(allow_localhost: true)
 
-require "coveralls"
-Coveralls.wear!
-
 require "ripcord"
+
+require 'simplecov'
+
+SimpleCov.start do
+  if ENV['CI']
+    require 'simplecov-lcov'
+
+    SimpleCov::Formatter::LcovFormatter.config do |c|
+      c.report_with_single_file = true
+      c.single_report_path = 'coverage/lcov.info'
+    end
+
+    formatter SimpleCov::Formatter::LcovFormatter
+  end
+
+  add_filter %w[version.rb]
+end
